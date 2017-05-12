be5341f3aa4209b60f8882ee00e448f7d84d6b86
bolek <arek2159@gmail.com>
2017-05-13 00:32:28 +0200

    init

--- index.php
+++ index.php
@@ -2,9 +2,118 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Title</title>
+  <title>Saper</title>
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+<div class="container">
 
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="#">Saper</a>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+          <li><a href="#">Link</a></li>
+        </ul>
+      </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+  </nav>
+
+  <section>
+
+    <table class="board2">
+      <tr>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+      </tr>
+      <tr>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+      </tr>
+      <tr>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+      </tr>
+      <tr>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+        <td><button><img src="res/flaga.png"></button></td>
+      </tr>
+    </table>
+
+    <hr/>
+
+    <table class="board">
+      <tr>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+      </tr>
+      <tr>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+      </tr>
+      <tr>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+      </tr>
+      <tr>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+        <td><img src="res/flaga.png"></td>
+      </tr>
+    </table>
+
+  </section>
+
+  <footer>
+    JS na NaN%
+  </footer>
+
+</div>
 </body>
 </html>
